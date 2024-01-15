75daae0bc52b8f4f87544545647095951f98ccef
ridhoakbarsyah <ridhoakbarsyah15@gmail.com>
2024-01-15 09:10:33 +0700

    Perbaikan validasi form

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -8,37 +8,37 @@
       <form action="{{ url('student') }}" method="post">
         {!! csrf_field() !!}
         <label>NIM</label></br>
-        <input type="number" name="nim" id="nim" class="form-control" autofocus></br>
+        <input type="number" name="nim" id="nim" class="form-control" autofocus value="{{ old('nim') }}"></br>
         @if($errors->has('nim'))
           <p class="text-danger">{{ $errors->first('nim') }}</p>
         @endif
 
         <label>Name</label></br>
-        <input type="text" name="name" id="name" class="form-control"></br>
+        <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}"></br>
         @if($errors->has('name'))
           <p class="text-danger">{{ $errors->first('name') }}</p>
         @endif
 
         <label>Email Address</label></br>
-        <input type="email" name="email" id="email" class="form-control"></br>
+        <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}"></br>
         @if($errors->has('email'))
           <p class="text-danger">{{ $errors->first('email') }}</p>
         @endif
 
         <label>City</label></br>
-        <input type="text" name="address" id="address" class="form-control"></br>
+        <input type="text" name="address" id="address" class="form-control" value="{{ old('address') }}"></br>
         @if($errors->has('address'))
           <p class="text-danger">{{ $errors->first('address') }}</p>
         @endif
 
         <label>Study Program</label></br>
-        <input type="text" name="program_study" id="program_study" class="form-control"></br>
+        <input type="text" name="program_study" id="program_study" class="form-control" value="{{ old('program_study') }}"></br>
         @if($errors->has('address'))
           <p class="text-danger">{{ $errors->first('program_study') }}</p>
         @endif
 
         <label>WhatsApp Number</label></br>
-        <input type="number" name="mobile" id="mobile" class="form-control"></br>
+        <input type="number" name="mobile" id="mobile" class="form-control" value="{{ old('mobile') }}"></br>
         @if($errors->has('address'))
           <p class="text-danger">{{ $errors->first('mobile') }}</p>
         @endif
